5a58351e346220828c0a2736a3e08f4c5603b1de
IamPawel <150140921+IamPawel@users.noreply.github.com>
2024-11-24 23:25:47 +0100

    documentation

--- Design_Patterns/library_iterator.py
+++ Design_Patterns/library_iterator.py
@@ -1,6 +1,6 @@
-# Singleton:
-# Zastosowanie: Zaimplementuj klasę LibraryCatalog, która zapewnia, że w programie istnieje tylko jedna instancja katalogu. Klasa ta będzie odpowiedzialna za przechowywanie i zarządzanie danymi o książkach.
-# Zadanie: Utwórz klasę Singleton i pokaż, jak uzyskać do niej dostęp z różnych części programu.
+# Iterator:
+# Zastosowanie: Zaimplementuj iterator do przeglądania listy książek w katalogu lub listy użytkowników. Może to być użyte do wyświetlania szczegółów książek lub informacji o użytkownikach w ustrukturyzowany sposób.
+# Zadanie: Stwórz wzorzec iteratora, który umożliwia iterację po kolekcji LibraryCatalog zawierającej książki lub UserManagement zarejestrowanych użytkowników.
 
 
 class LibraryCatalog:
@@ -9,12 +9,23 @@ class LibraryCatalog:
     def __new__(cls):
         if not cls._instance:
             cls._instance = super().__new__(cls)
-            cls._instance._initialize()  # private method to initialize the object
-        return cls._instance
+            cls._instance._initialize()
+            return cls._instance
 
     def _initialize(self):
         # initialize the object
-        self.books = []
+        self.books = [
+            {"title": "Book2", "author": "Author2", "is_available": True},
+            {"title": "Book1", "author": "Author1", "is_available": False},
+            {"title": "Book3", "author": "Author2", "is_available": True},
+            {"title": "Book4", "author": "Author1", "is_available": False},
+            {"title": "Book5", "author": "Author1", "is_available": True},
+            {"title": "Book6", "author": "Author3", "is_available": True},
+            {"title": "Book7", "author": "Author3", "is_available": True},
+            {"title": "Book8", "author": "Author3", "is_available": False},
+            {"title": "Book9", "author": "Author1", "is_available": True},
+            {"title": "Book10", "author": "Author2", "is_available": True},
+        ]
 
     def add_book(self, title, author, is_available=True):
         self.books.append(
@@ -55,47 +66,21 @@ class LibraryIterator:
             return book
         raise StopIteration
 
-    # all books
-    def all_books(self):
-        books = [book for book in self.books]
-        return print(books)
+    def filter_by_author(self, author):
+        filtered_books = [book for book in self.books if book["author"] == author]
+        return filtered_books
 
-    def available_books(self):
-        books = [book for book in self.books if book["is_available"]]
-        return print(books)
-
-    # search by title
-    def find_by_title(self, title):
-        books = [book for book in self.books if book["title"] == title]
-        return print(books)
-
-    # search by author
-    def find_by_author(self, author):
-        books = [book for book in self.books if book["author"] == author]
-        return print(books)
+    def filter_available_books(self):
+        available_books = [book for book in self.books if book["is_available"]]
+        return available_books
 
 
 # Usage
-library = LibraryCatalog()
-library.add_book("Book1", "Author1", is_available=True)
-library.add_book("Book2", "Author1", is_available=True)
-library.add_book("Book3", "Author2", is_available=False)
-library.add_book("Book4", "Author2", is_available=True)
-library.add_book("Book5", "Author1", is_available=False)
-library.add_book("Book6", "Author2", is_available=True)
-library.add_book("Book7", "Author1", is_available=False)
-library.add_book("Book8", "Author1", is_available=True)
-library.add_book("Book9", "Author1", is_available=True)
-
-
-# initialize iterator
-iter = LibraryIterator(library)
-
-print("All books:")
-iter.all_books()
+library_catalog = LibraryCatalog()
+print("Books by Author1:")
+for book in library_catalog.__iter__().filter_by_author("Author1"):
+    print(f"Title: {book['title']}, Author: {book['author']}")
 
 print("Available books:")
-iter.available_books()
-
-print("Books by Author1:")
-iter.find_by_author("Author1")
+for book in library_catalog.__iter__().filter_available_books():
+    print(f"Title: {book['title']}, Author: {book['author']}")
